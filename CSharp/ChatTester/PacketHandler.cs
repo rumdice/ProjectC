e7125df00091f93eb40a58e6e0db690ddf139695
rumdice <rumdice@gmail.com>
2021-08-05 11:21:54 +0900

    채팅서버 패킷 테스터 초안

---
+++ CSharp/ChatTester/PacketHandler.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.IO;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using ChatPacketStruct;
+
+namespace ChatTester
+{
+    public static class PacketHandler
+    {
+        public static Task Send(ClientWebSocket ws, string data, CancellationToken cancellation)
+        {
+            var encoded = Encoding.UTF8.GetBytes(data);
+            var buffer = new ArraySegment<byte>(encoded, 0, encoded.Length);
+            return ws.SendAsync(buffer, WebSocketMessageType.Text, true, cancellation);
+        }
+
+        public static async Task<string> Recv(ClientWebSocket ws)
+        {
+            var buffer = new ArraySegment<byte>(new byte[8192]);
+            using (var ms = new MemoryStream())
+            {
+                WebSocketReceiveResult result;
+                do
+                {
+                    result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+                    ms.Write(buffer.Array, buffer.Offset, result.Count);
+                }
+                while (!result.EndOfMessage);
+
+                ms.Seek(0, SeekOrigin.Begin);
+
+                using var reader = new StreamReader(ms, Encoding.UTF8);
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static async Task PacketProcAsync(ClientWebSocket ws, string cmd)
+        {
+            string packetStr = "";
+
+            switch (cmd)
+            {
+                case "login":
+                    {
+                        var reqbody = new RequestBodyLogin
+                        {
+                            userUid = "aabbccdd123",
+                        };
+
+                        var req = new RequestLogin
+                        {
+                            body = reqbody,
+                        };
+
+                        packetStr = JsonConvert.SerializeObject(req);
+                        break;
+                    }
+
+                case "test":
+                    {
+                        break;
+                    }
+
+                case "chat":
+                    {
+                        Console.WriteLine("Input Chat msg:");
+                        var msg = Console.ReadLine();
+
+                        var reqbody = new RequestBodyChat
+                        {
+
+                        };
+
+                        var req = new RequestChat
+                        {
+                            body = reqbody,
+                        };
+
+                        packetStr = JsonConvert.SerializeObject(req);
+                        break;
+                    }
+
+                case "":
+                    {
+                        break;
+                    }
+
+                default:
+                    break;
+            }
+
+            await Send(ws, packetStr, CancellationToken.None);
+            Console.WriteLine("[Send] MSG:{0}", JsonConvert.ToString(packetStr));
+
+            var recv = await Recv(ws);
+            Console.WriteLine("[Recv] MSG:{0}", recv);
+        }
+    }
+}
---
+++ CSharp/ChatTester/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChatTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = TestClient();
+            t.Wait();
+        }
+
+        static async Task TestClient()
+        {
+            var ws = new ClientWebSocket();
+            var uri = new Uri("ws://localhost:8080");
+
+            // connect to server
+            await ws.ConnectAsync(uri, CancellationToken.None);
+
+            var conn = (Convert.ToString(ws.State) != "Open") ? "Fail!" : "Success";
+            Console.WriteLine($"Url:{uri}, Connect:{conn}");
+
+            // 패킷 실행
+            while (true)
+            {
+                Console.WriteLine("Input Packet Header (exit is end) :"); // 커맨드 입력 -> 해당 컨텐츠 테스트
+
+                var cmd = Console.ReadLine();
+                await PacketHandler.PacketProcAsync(ws, cmd);
+
+                if (cmd == "exit")
+                {
+                    // disconnect to server
+                    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Disconnect Normal", CancellationToken.None);
+                    Console.WriteLine("Client Disconnect");
+
+                    Console.WriteLine("Match Test End");
+                    break;
+                }
+            }
+        }
+    }
+}
