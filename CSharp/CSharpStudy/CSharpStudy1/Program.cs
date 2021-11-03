41aef2cdf7373bce984d1a72104be937c356dc95
rumdice <77256404+Aintegla@users.noreply.github.com>
2021-11-03 09:30:19 +0900

    예제 1 작성 - 메인함수에서 스레드를 생성하고 실행할 메서드를 전달한다. - 스레드가 생성되면 전달받은 객체를 생성하고 수행한다. - 실행시 결과가 조금씩 다르다. 메인과 생성된 스레드에서 동시에 수행되기 때문이다.

--- CSharp/CSharpStudy/CSharpStudy1/Program.cs
+++ CSharp/CSharpStudy/CSharpStudy1/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Threading;
 
 namespace CSharpStudy1
 {
     class Program
     {
+        static void PrintNums()
+        {
+            Console.WriteLine("Start..");
+            for (int i = 1; i < 10; i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
+            var t = new Thread(PrintNums);
+            t.Start();
+            PrintNums();
         }
     }
 }
